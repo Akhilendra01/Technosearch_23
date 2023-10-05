e372769d69a07a69b001a4c8989f6538dd1f0438
Ankit Kushwaha <ankitkshwh003@gmail.com>
2023-10-05 23:04:43 +0530

    Readding footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -10,7 +10,7 @@ function Layout() {
         <DrawerAppBar />
         <Landing />
         <About />
-        {/* <Footer/> */}
+        <Footer/>
       {/* </CustomThemeProvider> */}
     </>
   )
